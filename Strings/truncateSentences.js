9ade770dfa8521dc616f6d34519fbe9b22379956
poseidon139 <surya03111997@gmail.com>
2023-12-15 01:20:16 +0530

    truncateSentences

---
+++ Strings/truncateSentences.js
@@ -0,0 +1,10 @@
+var truncateSentence = function(s, k) {
+    let res="";
+    let words=s.split(' ');
+    for (i =0; i < k ; i++){
+        res+=words[i]+" ";
+    }
+    return res.trim();
+   };
+
+console.log(truncateSentence("Hello how are you Contestant",4))

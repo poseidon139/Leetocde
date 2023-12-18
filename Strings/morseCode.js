a2654e1d2b87e95147a432380d464325e46cb000
poseidon139 <surya03111997@gmail.com>
2023-12-19 00:52:08 +0530

    Morse Code

---
+++ Strings/morseCode.js
@@ -0,0 +1,15 @@
+var uniqueMorseRepresentations = function(words) {
+    let morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+
+    let set= new Set();
+    for(i of words){
+        let morseWord="";
+        for ( j of i ){
+            morseWord+=morse[j.charCodeAt(0)-97];
+        }
+        set.add(morseWord)
+    }
+    return set.size;
+};
+
+console.log(uniqueMorseRepresentations(["gin","zen","gig","msg"]));

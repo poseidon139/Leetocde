579eed04681dbd34292c0ea1c701078b678466f1
Surya Vardhan <105297665+poseidon139@users.noreply.github.com>
2024-08-05 17:00:06 +0000

    added good pair

--- Arrays/goodPairs.java
+++ Arrays/goodPairs.java
@@ -1,6 +1,12 @@
+
+
 // Leetcode #1512 
 
 public class goodPairs {
+    public int numIdenticalPairs(int[] nums) {
+         
+    }
+    
     public static void main(String args[]){
         System.out.println("jhbcfan");
     }
